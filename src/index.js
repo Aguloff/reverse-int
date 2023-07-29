791d4e995acbf80621d696d065be98ed5c2480f6
Aguloff <agulovu@gmail.com>
2023-07-29 20:40:33 +0300

    add complited task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  n = Math.abs(n).toString();
+
+  for (let i = n.length - 1; i >= 0; i--) {
+    result += n[i];
+  }
+
+  return Number(result);
 }
